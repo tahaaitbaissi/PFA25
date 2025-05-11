ed14689db2eaa9bff1578568851adac55cc3b78b
Elkarmi Amine <karmi0amine@gmail.com>
2025-05-11 19:51:22 +0100

    Modification de Logo

--- frontend/src/components/test/userData.js
+++
@@ -1,9 +0,0 @@
-export const userData = {
-    user: {
-      categories: ["Technologie", "Actualités", "Science"],
-      email: "user1@example.com",
-      points: 1500,
-      role: "admin",
-      username: "Taha_Dev"
-    }
-  };
